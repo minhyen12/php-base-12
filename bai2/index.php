14cdb51c374bb2285c87ec2bea0197104f868d69
Xuan Khuong <phungyen98act@gmail.com>
2021-08-31 20:10:18 +0700

    validate string

---
+++ bai2/index.php
@@ -0,0 +1,37 @@
+<?php
+    function checkString($str) {
+	    if ((strpos($str, 'restaurant') !== false && strpos($str, 'book') === false) || (strpos($str, 'restaurant') === false && strpos($str, 'book') !== false)) {
+		    return true;
+	    }
+	    return false;
+    }
+
+    function countWord($str) {
+        $strArr = explode(".", $str); 
+        $count = count($strArr); 
+        foreach ($strArr as $item) {
+            if (empty($item) && $count > 0) {
+                $count--;
+            }
+        }
+        return $count;
+    }
+
+    $filename = ['file1.txt', 'file2.txt'] ;
+    foreach ($filename as $key => $value) {
+        $myfile = fopen($value, 'r');
+        $connect = fread($myfile, filesize($value));
+        if(checkString($connect) === true) {
+            $countWord = countWord($connect);
+            echo "chuoi hop le<br>";
+            echo "Số câu trong chuỗi: ".$countWord."<br>";
+        }
+        else {
+            echo 'chuoi khong hop le<br>';
+        }
+        fclose($myfile);
+        if ($key < count($filename) - 1) {
+            echo "======================================<br>";
+        }
+    }
+?> 
